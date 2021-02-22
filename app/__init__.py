bae07d18e271624c74fc24d32b648e8756bd35bb
Jackson-coder-arch <jacksonikonya@gmail.com>
2021-02-23 00:29:16 +0300

    config from object

--- app/__init__.py
+++ app/__init__.py
@@ -1,24 +1,24 @@
-# from flask import Flask
-# # from flask_bootstrap import Bootstrap
-# from config import config_options
+from flask import Flask
+# from flask_bootstrap import Bootstrap
+from config import config_options
 
-# # bootstrap = Bootstrap()
+# bootstrap = Bootstrap()
 
-# def create_app(config_name):
-#   app = Flask(__name__)
+def create_app(config_name):
+  app = Flask(__name__)
 
-#   #create app configurations
-#   app.config.from_object(config_options[config_name])
+  #create app configurations
+  app.config.from_object(config_options[config_name])
 
-#   # initialize flask extentions
-#   # bootstrap.init_app(app)
+  # initialize flask extentions
+  # bootstrap.init_app(app)
 
-#   #register blueprint
-#   from .main import main as main_blueprint
-#   app.register_blueprint(main_blueprint)
+  #register blueprint
+  from .main import main as main_blueprint
+  app.register_blueprint(main_blueprint)
 
-#   #setting config
-#   from .request import configure_request
-#   configure_request(app)
+  #setting config
+  from .request import configure_request
+  configure_request(app)
 
-#   return app
+  return app
